3f17d08ea7f95ad3b56ef516baf87911a06ec349
Thomas Haukland <thomas.haukland@gmail.com>
2024-02-09 12:18:32 +0100

    DemoApp: Use nuget framework reference instead of project reference
    Also add .idea folder to .gitignore to allow easier edits in Rider

---
+++ samples/DemoApp/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using Microsoft.Maui;
+global using Microsoft.Maui.Hosting;
+
+
+
